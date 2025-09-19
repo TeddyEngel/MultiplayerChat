e3f99c9b6e19e2b2c67dd240bd8b6a0d35df4793
Teddy Engel <engel.teddy@gmail.com>
2025-09-19 18:08:22 +0100

    Initial commit including cmake configuration, sublime configuration, hello world mains for client and server and tests for client and server

---
+++ client/tests/src/main_test.c
@@ -0,0 +1,29 @@
+#include "unity.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void Main_WhenExecuted_OutputsHelloWorld(void) {
+    FILE *fp;
+    char output[100];
+    
+    fp = popen("../client/build/MultiplayerChat_Client", "r");
+    TEST_ASSERT_NOT_NULL(fp);
+    
+    fgets(output, sizeof(output), fp);
+    pclose(fp);
+    
+    TEST_ASSERT_EQUAL_STRING("Hello world", output);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(Main_WhenExecuted_OutputsHelloWorld);
+    return UNITY_END();
+}
